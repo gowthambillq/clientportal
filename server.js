018d1bd13eee75cf9d88414313dd20ee531d2048
gowthambillq <gowtham@billq.org>
2025-09-29 08:57:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://clientportal.billq.org",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(bodyParser.json({ limit: '10mb' }));
 
 const transporter = nodemailer.createTransport({
